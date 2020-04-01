570538d6664e6ad0d8045a0fc2692c355a1cb3b5
Clint Troxel <clinton.troxel@gsa.gov>
2020-04-01 17:42:44 -0600

    format common flows

--- load_testing/common_flows.py
+++ load_testing/common_flows.py
@@ -9,6 +9,8 @@ import os
 """
 *** Sign In Flow ***
 """
+
+
 def do_sign_in(context):
 
     # This should match the number of users that were created for the DB with the rake task
@@ -54,7 +56,7 @@ def do_sign_in(context):
         return
 
     code_form = dom.find("form[action='/login/two_factor/sms']")
-    
+
     resp = context.client.post(
         code_form.attr('action'),
         data={
@@ -74,80 +76,82 @@ def do_sign_in(context):
 """
 *** Sign Up Flow ***
 """
+
+
 def do_sign_up(context):
-  fake = Faker()
-  phone_numbers = foney.phone_numbers()
-  default_password = "salty pickles"
+    fake = Faker()
+    phone_numbers = foney.phone_numbers()
+    default_password = "salty pickles"
 
-  # GET the new email page
-  resp = context.client.get("/sign_up/enter_email")
-  dom = resp_to_dom(resp)
+    # GET the new email page
+    resp = context.client.get("/sign_up/enter_email")
+    dom = resp_to_dom(resp)
 
-  # Post fake email and get confirmation link (link shows up in "load test mode")
-  new_email = "test+{}@test.com".format(fake.md5())
-  resp = context.client.post("/sign_up/enter_email",
-            data={
-              'user[email]': new_email,
-              'authenticity_token': authenticity_token(resp),
-            },
-            catch_response=True)
+    # Post fake email and get confirmation link (link shows up in "load test mode")
+    new_email = "test+{}@test.com".format(fake.md5())
+    resp = context.client.post("/sign_up/enter_email",
+                               data={
+                                   'user[email]': new_email,
+                                   'authenticity_token': authenticity_token(resp),
+                               },
+                               catch_response=True)
 
-  with resp as confirm_resp:
-      dom = resp_to_dom(confirm_resp)
-      try:
-          confirmation_link = dom.find("#confirm-now")[0].attrib['href']
-      except Exception:
-          confirm_resp.failure(
-              "Could not find CONFIRM NOW link, is IDP enable_load_testing_mode: 'true' ?")
-  
-  # Get confirmation token
-  resp = context.client.get(confirmation_link, name="/sign_up/email/confirm?confirmation_token=")
-  dom = resp_to_dom(resp)
-  token = dom.find('[name="confirmation_token"]:first').attr('value')
+    with resp as confirm_resp:
+        dom = resp_to_dom(confirm_resp)
+        try:
+            confirmation_link = dom.find("#confirm-now")[0].attrib['href']
+        except Exception:
+            confirm_resp.failure(
+                "Could not find CONFIRM NOW link, is IDP enable_load_testing_mode: 'true' ?")
 
-  # Set user password
-  resp = context.client.post("/sign_up/create_password",
-            data={
-                'password_form[password]': default_password,
-                'authenticity_token': authenticity_token(resp),
-                'confirmation_token': token,
-            }
-        )
+    # Get confirmation token
+    resp = context.client.get(
+        confirmation_link, name="/sign_up/email/confirm?confirmation_token=")
+    dom = resp_to_dom(resp)
+    token = dom.find('[name="confirmation_token"]:first').attr('value')
 
-  # After password creation set up SMS 2nd factor
-  phone_setup_url = "/phone_setup"
-  resp = context.client.get(phone_setup_url)
-  dom = resp_to_dom(resp)
-  auth_token = authenticity_token(resp)
+    # Set user password
+    resp = context.client.post("/sign_up/create_password",
+                               data={
+                                   'password_form[password]': default_password,
+                                   'authenticity_token': authenticity_token(resp),
+                                   'confirmation_token': token,
+                               }
+                               )
 
-  resp = context.client.post(
-    phone_setup_url,
-    data={
-      '_method': 'patch',
-      'new_phone_form[international_code]': 'US',
-      'new_phone_form[phone]': phone_numbers[randint(1, 1000)],
-      'new_phone_form[otp_delivery_preference]': 'sms',
-      'authenticity_token': auth_token,
-      'commit': 'Send security code',
-    },
-    catch_response=True
-  )
+    # After password creation set up SMS 2nd factor
+    phone_setup_url = "/phone_setup"
+    resp = context.client.get(phone_setup_url)
+    dom = resp_to_dom(resp)
+    auth_token = authenticity_token(resp)
 
-  with resp as otp_resp:
-      dom = resp_to_dom(otp_resp)
-      try:
-          otp_code = dom.find('input[name="code"]')[0].attrib['value']
-      except Exception as err:
-          resp.failure(
-              "Could not find pre-filled OTP code, is IDP telephony_disabled: 'true' ?")
-          return
+    resp = context.client.patch(
+        phone_setup_url,
+        data={
+            'new_phone_form[international_code]': 'US',
+            'new_phone_form[phone]': phone_numbers[randint(1, 1000)],
+            'new_phone_form[otp_delivery_preference]': 'sms',
+            'authenticity_token': auth_token,
+            'commit': 'Send security code',
+        },
+        catch_response=True
+    )
 
-  # Visit security code page and submit pre-filled OTP
-  resp = context.client.post(
-      "/login/two_factor/sms",
-      data={
-          'code': otp_code,
-          'authenticity_token': authenticity_token(resp),
-      })
+    with resp as otp_resp:
+        dom = resp_to_dom(otp_resp)
+        try:
+            otp_code = dom.find('input[name="code"]')[0].attrib['value']
+        except Exception:
+            resp.failure(
+                "Could not find pre-filled OTP code, is IDP telephony_disabled: 'true' ?")
+            return
 
-  return resp
+    # Visit security code page and submit pre-filled OTP
+    resp = context.client.post(
+        "/login/two_factor/sms",
+        data={
+            'code': otp_code,
+            'authenticity_token': authenticity_token(resp),
+        })
+
+    return resp
