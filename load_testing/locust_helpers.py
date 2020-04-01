57db984b8b39192d80ae03a1c0bb54aff7e53369
Clint Troxel <11878+ctro@users.noreply.github.com>
2020-04-01 17:44:29 -0600

    IAL2 load testing flow (#4)

--- load_testing/locust_helpers.py
+++ load_testing/locust_helpers.py
@@ -1,18 +1,44 @@
 import pyquery
 from random import randint
 
-# Functions that are used across different locustfiles
-def authenticity_token(dom, id=None):
+# Utility functions that are used across different locustfiles
+
+def do_request(context, method, path, expected_redirect=None, data={}, files={}):
+   with getattr(context.client, method)(
+        path, 
+        headers=desktop_agent_headers(),
+        data=data, 
+        files=files,
+        catch_response=True) as resp:
+            if expected_redirect:
+                verify_resp_url(expected_redirect, resp)
+            return resp
+
+def authenticity_token(response, index=0):
     """
     Retrieves the CSRF auth token from the DOM for submission.
     If you need to differentiate between multiple CSRF tokens on one page,
-    pass the optional ID of the parent form (with hash)
+    pass the optional index of the CSRF on the page
     """
-    selector = 'input[name="authenticity_token"]:first'
+    selector = 'input[name="authenticity_token"]'
 
-    if id:
-        selector = '{} {}'.format(id, selector)
-    return dom.find(selector).attr('value')
+    dom = resp_to_dom(response)
+    token = dom.find(selector).eq(index).attr('value')
+    # print("Returning authenticity_token: {}".format(token))
+
+    return token
+
+def otp_code(response):
+    """
+    Retrieves the auto-populated OTP code from the DOM for submission.
+    """
+    selector = 'input[name="code"]'
+
+    dom = resp_to_dom(response)
+    code = dom.find(selector).attr('value')
+    # print("Returning OTP code: {}".format(code))
+
+    return code
 
 def resp_to_dom(resp):
     """
@@ -42,3 +68,26 @@ def random_cred(num_users):
     }
 
     return credential
+
+"""
+Format a common error message with full content attached
+"""
+def err_msg(msg, resp):
+    return """"
+           {}
+           Our current URL is: {}
+           Content is: {}.
+           """.format(msg, resp.url, resp.content)
+
+"""
+Use this in headers to act as a Desktop
+"""
+def desktop_agent_headers():
+    return {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"}
+
+"""
+Raise errors when you are not at the expected page
+"""
+def verify_resp_url(url, resp):
+    if url not in resp.url:
+        resp.failure("You wanted {}, but got {} for a url".format(url, resp.url))
