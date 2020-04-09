9b30d3903663668d0159e17a2abefba0f1796bbf
Clint Troxel <clinton.troxel@gsa.gov>
2020-04-09 08:52:23 -0600

    move to module

--- load_testing/sp_sign_in.locustfile.py
+++ load_testing/sp_sign_in.locustfile.py
@@ -1,83 +1,12 @@
 from locust import HttpLocust, TaskSet, task, between
-from common_flows import flow_helper
+from common_flows import flow_sp_sign_in
 
 
 class SPSignInLoad(TaskSet):
     @task(1)
     def sp_sign_in_load_test(self):
-
-        sp_root_url = flow_helper.get_env("SP_HOST")
-
-        # GET the SP root, which should contain a login link, give it a friendly name for output
-        resp = flow_helper.do_request(
-            self, "get", sp_root_url, sp_root_url, {}, {}, sp_root_url
-        )
-        signin_link = flow_helper.sp_signin_link(resp)
-
-        # GET the signin link we found
-        resp = flow_helper.do_request(
-            self,
-            "get",
-            signin_link,
-            "/?request_id=",
-            {},
-            {},
-            flow_helper.url_without_querystring(signin_link),
-        )
-        auth_token = flow_helper.authenticity_token(resp)
-        request_id = flow_helper.querystring_value(resp.url, "request_id")
-
-        # POST username and password
-        num_users = flow_helper.get_env("NUM_USERS")
-        credentials = flow_helper.random_cred(num_users)
-        resp = flow_helper.do_request(
-            self,
-            "post",
-            "/",
-            "/login/two_factor/sms",
-            {
-                "user[email]": credentials["email"],
-                "user[password]": credentials["password"],
-                "user[request_id]": request_id,
-                "authenticity_token": auth_token,
-            },
-        )
-        auth_token = flow_helper.authenticity_token(resp)
-        code = flow_helper.otp_code(resp)
-
-        # POST to 2FA
-        # If first time for user, this redirects to "completed", otherwise to the SP root.
-        resp = flow_helper.do_request(
-            self,
-            "post",
-            "/login/two_factor/sms",
-            None,
-            {"code": code, "authenticity_token": auth_token,},
-        )
-
-        if "/sign_up/completed" in resp.url:
-            # POST to completed, should go back to the SP
-            auth_token = flow_helper.authenticity_token(resp)
-            resp = flow_helper.do_request(
-                self,
-                "post",
-                "/sign_up/completed",
-                sp_root_url,
-                {"authenticity_token": auth_token,},
-            )
-
-        # We should now be at the SP root, with a "logout" link.
-        # Test SP goes back to the root, so we'll test that for now
-        logout_link = flow_helper.sp_signout_link(resp)
-        flow_helper.do_request(
-            self,
-            "get",
-            logout_link,
-            sp_root_url,
-            {},
-            {},
-            flow_helper.url_without_querystring(logout_link),
-        )
+        # This flow does its own SP logout
+        flow_sp_sign_in.do_sp_sign_in(self)
 
 
 class WebsiteUser(HttpLocust):
