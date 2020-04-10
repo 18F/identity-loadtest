695ee15cd6dced785816cde530ab2a4233548596
Clint Troxel <clinton.troxel@gsa.gov>
2020-04-10 09:31:29 -0600

    formatting

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,16 +1,18 @@
 from unittest.mock import MagicMock
 
+
 def mock_response(fixture_name):
 
-  """
+    """
   Accepts the name of a file in the fixtures directory
   Returns a mocked response object
   """
 
-  f = open("tests/fixtures/" + fixture_name, "r")
-  fixture_content = f.read()
+    f = open("tests/fixtures/" + fixture_name, "r")
+    fixture_content = f.read()
 
-  response = MagicMock()
-  response.content = fixture_content
+    response = MagicMock()
+    response.content = fixture_content
 
-  return response
+    return response
+
