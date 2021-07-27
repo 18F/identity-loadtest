67eb39eab2e84f13aff1f777d46ac893a15a0ce3
Justin Grevich <justin@grevi.ch>
2021-07-27 12:32:58 -0700

    Dedicated flows for SP based IAL2 flows

---
+++ load_testing/sp_ial2_sign_up.locustfile.py
@@ -0,0 +1,17 @@
+from locust import HttpUser, TaskSet, task, between
+from common_flows import flow_sp_ial2_sign_up, flow_helper
+
+
+class SP_IAL2_SignUpLoad(TaskSet):
+    # Preload drivers license data
+    license_front = flow_helper.load_fixture("mont-front.jpeg")
+    license_back = flow_helper.load_fixture("mont-back.jpeg")
+
+    @task(1)
+    def sp_sign_in_load_test(self):
+        flow_sp_ial2_sign_up.ial2_sign_up(self)
+
+
+class WebsiteUser(HttpUser):
+    tasks = [SP_IAL2_SignUpLoad]
+    wait_time = between(5, 9)
