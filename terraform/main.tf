e3acb9188e4c773200b0bb5fd407d89350ceca91
timothy-spencer <timothy.spencer@gsa.gov>
2022-09-27 18:04:58 -0700

    need to init the backend properly

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,12 +5,7 @@ provider "aws" {
 
 terraform {
   required_version = ">= 1.2.4"
-  backend "s3" {
-    bucket = "login-gov.tf-state.${data.aws_caller_identity.current.account_id}-${var.region}"
-    key    = "terraform-loadtest/terraform-${var.cluster_name}.tfstate"
-    region = var.region
-    dynamodb_table = "terraform_locks"
-  }
+  backend "s3" {}
 }
 
 # Using these data sources allows the configuration to be
