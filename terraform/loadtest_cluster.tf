09c352513db8deb3da01eb49cf72e6209236cd98
timothy-spencer <timothy.spencer@gsa.gov>
2022-10-24 09:15:48 -0700

    turn off spot so we have node stability

--- terraform/loadtest_cluster.tf
+++ terraform/loadtest_cluster.tf
@@ -17,17 +17,17 @@ module "loadtest" {
 
   # EKS MANAGED NODE GROUPS
   managed_node_groups = {
-    spot = {
-      node_group_name = "${var.cluster_name}-managed-spot"
-      min_size        = 1
-      max_size        = 40
-      desired_size    = 2
-      subnet_ids      = module.vpc.private_subnets
-      capacity_type   = "SPOT"
-      instance_types  = ["m5.large", "m4.large", "m6a.large", "m5a.large", "m5d.large"]    // Instances with same specs for memory and CPU so Cluster Autoscaler scales efficiently
-      disk_size       = 100                                                                # disk_size will be ignored when using Launch Templates  
-      k8s_taints      = [{key= "spot", value="true", effect="NO_SCHEDULE"}]
-    }
+    # spot = {
+    #   node_group_name = "${var.cluster_name}-managed-spot"
+    #   min_size        = 1
+    #   max_size        = 40
+    #   desired_size    = 2
+    #   subnet_ids      = module.vpc.private_subnets
+    #   capacity_type   = "SPOT"
+    #   instance_types  = ["m5.large", "m4.large", "m6a.large", "m5a.large", "m5d.large"]    // Instances with same specs for memory and CPU so Cluster Autoscaler scales efficiently
+    #   disk_size       = 100                                                                # disk_size will be ignored when using Launch Templates  
+    #   k8s_taints      = [{key= "spot", value="true", effect="NO_SCHEDULE"}]
+    # }
     ondemand = {
       node_group_name = "${var.cluster_name}-managed-ondemand"
       min_size        = 2
