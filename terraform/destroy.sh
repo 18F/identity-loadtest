88f1912dfa6cb080871db4a7b7cfbc570f6c5216
timothy-spencer <timothy.spencer@gsa.gov>
2022-10-31 15:44:20 -0700

    change the way the loadtest zone works so that we don't step on each other

--- terraform/destroy.sh
+++ terraform/destroy.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 #
-# This script deploys the environment.
+# This script destroys the environment.
 # 
 set -e
 
 if [ -z "$1" ]; then
-     echo "usage:   $0 <cluster_name> [-s]"
-     echo "            -s means do an initial setup.  You only need to do this the first time you set up a cluster."
-     echo "example: $0 loadtest -s"
-     echo "example: $0 loadtest"
+     echo "usage:   $0 <cluster_name>"
+     echo "example: $0 loadtest2"
      exit 1
 else
      export TF_VAR_cluster_name="$1"
@@ -27,11 +25,6 @@ terraform init -backend-config="bucket=$BUCKET" \
       -backend-config="dynamodb_table=terraform_locks" \
       -backend-config="region=$TF_VAR_region"
 
-# initial setup steps to make sure there's no dependency problems
-if [ "$2" = "-s" ] ; then
-	terraform apply -target="module.vpc" -target="data.aws_route53_zone.primary"
-	terraform apply -target="module.loadtest"
-fi
+# burn it downnnnnnnn
+terraform destroy
 
-# normal deploy
-terraform apply
