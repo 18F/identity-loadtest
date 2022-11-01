e8826982377c03bb61d927f63f629cc41cfad443
timothy-spencer <timothy.spencer@gsa.gov>
2022-11-01 15:38:27 -0700

    shrink us down

--- terraform/loadtest_cluster.tf
+++ terraform/loadtest_cluster.tf
@@ -31,7 +31,7 @@ module "loadtest" {
     ondemand = {
       node_group_name = "${var.cluster_name}-managed-ondemand"
       min_size        = 4
-      max_size        = 40
+      max_size        = 48
       desired_size    = 6
       subnet_ids      = module.vpc.private_subnets
       capacity_type   = "ON_DEMAND"
