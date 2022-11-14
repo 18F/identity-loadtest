d517c7f4fee72f2545a61826cf57885b1bbc1572
timothy-spencer <timothy.spencer@gsa.gov>
2022-11-14 10:40:12 -0800

    add comments to scaleup

--- scaleup.sh
+++ scaleup.sh
@@ -6,6 +6,8 @@
 #   aws-vault exec sandbox-admin -- aws eks --region us-west-2  update-kubeconfig --name clustername
 #
 
+echo "make sure the idp, worker, and obproxy ASGs have the proper number of instances"
+echo "make sure the idp and worker dbs are scaled up properly"
 echo "scale the nodes in the cluster up to 126 or so, and the max to 250"
 echo "change the locust-worker replicas in locust.yaml to 350, push it up there and wait until 350 are running"
 
