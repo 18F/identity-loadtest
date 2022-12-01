3f671973ab1b631541799b4ccfbbd4c0d714f37f
timothy-spencer <timothy.spencer@gsa.gov>
2022-12-01 14:34:46 -0800

    change back to full prod simulator

--- scaleup.sh
+++ scaleup.sh
@@ -19,5 +19,5 @@ read line
 echo "scaling up pods"
 aws-vault exec sandbox-admin -- kubectl scale deployment coredns --replicas=51 --namespace=kube-system
 aws-vault exec sandbox-admin -- kubectl scale deployment oidc-sinatra --replicas=1000 --namespace=oidc-sinatra
-aws-vault exec sandbox-admin -- kubectl scale deployment fake-server --replicas=1000 --namespace=fake-server
+aws-vault exec sandbox-admin -- kubectl scale deployment fake-server --replicas=1500 --namespace=fake-server
 
