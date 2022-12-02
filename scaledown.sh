07c21d65047d54f7bb741de89ce5c6e434b38f06
timothy-spencer <timothy.spencer@gsa.gov>
2022-12-02 09:39:23 -0800

    scale up

--- scaledown.sh
+++ scaledown.sh
@@ -7,6 +7,7 @@
 echo "scaling down pods"
 aws-vault exec sandbox-admin -- kubectl scale deployment coredns --replicas=10 --namespace=kube-system
 aws-vault exec sandbox-admin -- kubectl scale deployment oidc-sinatra --replicas=1 --namespace=oidc-sinatra
+aws-vault exec sandbox-admin -- kubectl scale deployment fake-server --replicas=1 --namespace=fake-server
 
 echo "scale the nodes in the cluster down to 6 or so, and make the max be 10."
 echo "change the locust-worker replicas in locust.yaml to 5, push it up there."
