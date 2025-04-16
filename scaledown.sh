56fd1245ca8b44e9695838f655ea7cfee2b5794f
Justin Grevich <justin@grevi.ch>
2025-04-16 15:05:11 -0700

    adjusted for values needed for 19200 users

--- scaledown.sh
+++ scaledown.sh
@@ -5,10 +5,14 @@
 #
 
 echo "scaling down pods"
+
+aws-vault exec sandbox-admin -- kubectl scale deployment argo-cd-argocd-server --replicas=1 --namespace=argocd
+aws-vault exec sandbox-admin -- kubectl scale deployment argo-cd-argocd-repo-server --replicas=1 --namespace=argocd
 aws-vault exec sandbox-admin -- kubectl scale deployment coredns --replicas=2 --namespace=kube-system
-aws-vault exec sandbox-admin -- kubectl scale deployment oidc-sinatra --replicas=1 --namespace=oidc-sinatra
+aws-vault exec sandbox-admin -- kubectl scale deployment pt-oidc-sinatra --replicas=1 --namespace=pt
 aws-vault exec sandbox-admin -- kubectl scale deployment fake-server --replicas=1 --namespace=fake-server
+aws-vault exec sandbox-admin -- kubectl scale deployment pt-locust-worker --replicas=1 --namespace=pt
+aws-vault exec sandbox-admin -- kubectl scale deployment pt-locust-master --replicas=1 --namespace=pt
 
 echo "scale the nodes in the cluster down to 6 or so, and make the max be 10."
 echo "change the locust-worker replicas in locust.yaml to 5, push it up there."
-
