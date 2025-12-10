4704883f4cba798770c44c92f7c73e03a14bc2a2
Justin Grevich <justin@grevi.ch>
2025-12-10 13:55:58 -0800

    odd, somehoww I missed a test.com in the global search.

--- load_testing/lib/flow_sign_up.py
+++ load_testing/lib/flow_sign_up.py
@@ -17,7 +17,7 @@ from .flow_helper import (
 def do_sign_up(context):
     context.client.cookies.clear()
     fake = Faker()
-    new_email = "test+{}@test.com".format(fake.md5())
+    new_email = "test+{}@example.com".format(fake.md5())
     default_password = "salty pickles"
 
     # GET the new email page
